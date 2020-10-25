adaa178fc25ba58fe2b128c8e5d2eb5be66058bf
Juan Saldana <saldanaj97@gmail.com>
2020-10-25 14:06:33 -0500

    File structure updates

---
+++ Hwk2/HW2_Q1.py
@@ -0,0 +1,14 @@
+import random
+import sys
+
+list1 = []
+file = open("set9_10.txt", "w")
+for n in range(1):
+    for i in range(1000000):
+       list1.append(random.randint(0,10000))
+    for i in list1:
+        file.write(str(i))
+        file.write("\n")
+    list1.clear()
+
+file.close
